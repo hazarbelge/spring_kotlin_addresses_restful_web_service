700378ecec52e0ac34991cedd239cee309d678b0
Hazar Belge <hazarbelge@gmail.com>
2022-02-02 04:52:21 +0100

    Add city.mustache and edit html controller

--- src/main/kotlin/com/hazarbelge/addresses/controller/HtmlController.kt
+++ src/main/kotlin/com/hazarbelge/addresses/controller/HtmlController.kt
@@ -1,8 +1,11 @@
 package com.hazarbelge.addresses.controller
 
 import com.hazarbelge.addresses.base.BaseController
+import com.hazarbelge.addresses.model.City
 import com.hazarbelge.addresses.model.Country
+import com.hazarbelge.addresses.repository.CityRepository
 import com.hazarbelge.addresses.repository.CountryRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -12,23 +15,12 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.server.ResponseStatusException
 
 @Controller
-class HtmlController(private val countryRepository: CountryRepository) : BaseController() {
+class HtmlController(private val countryRepository: CountryRepository, private val cityRepository: CityRepository) : BaseController() {
     @GetMapping("/")
-    fun addresses(model: Model): String {
+    fun address(model: Model): String {
         model["title"] = "Addresses"
         model["countries"] = countryRepository.findAllByOrderByNameAsc().map { it.render() }
         return "address"
-    }
-
-    @GetMapping("/country/{code}")
-    fun country(@PathVariable code: String, model: Model): String {
-        val country = countryRepository.findByCode(code)?.render() ?: throw ResponseStatusException(
-            HttpStatus.NOT_FOUND,
-            "This country does not exist"
-        )
-        model["title"] = country.code
-        model["country"] = country
-        return "country"
     }
 
     fun Country.render() = RenderedCountry(
@@ -44,4 +36,39 @@ class HtmlController(private val countryRepository: CountryRepository) : BaseCon
         val code: String,
         val continent: String,
     )
+
+    @GetMapping("/country/{code}")
+    fun country(@PathVariable code: String, model: Model): String {
+        val country = countryRepository.findByCode(code)?.render() ?: throw ResponseStatusException(
+            HttpStatus.NOT_FOUND,
+            "This country does not exist"
+        )
+        model["title"] = country.code
+        model["country"] = country
+        model["cities"] = cityRepository.findAllByCountryOrderByNameAsc(countryRepository.findByCode(code)!!).map { it.render() }
+        return "country"
+    }
+
+    fun City.render() = RenderedCity(
+        id,
+        name,
+        country,
+    )
+
+    data class RenderedCity(
+        val id: Long?,
+        val name: String,
+        val country: Country,
+    )
+
+    @GetMapping("/country/{code}/city/{id}")
+    fun city(@PathVariable code: String, @PathVariable id: Long, model: Model): String {
+        val city = cityRepository.findByIdOrNull(id)?.render() ?: throw ResponseStatusException(
+            HttpStatus.NOT_FOUND,
+            "This city does not exist"
+        )
+        model["title"] = city.name
+        model["city"] = city
+        return "city"
+    }
 }
--- src/main/kotlin/com/hazarbelge/addresses/repository/CityRepository.kt
+++ src/main/kotlin/com/hazarbelge/addresses/repository/CityRepository.kt
@@ -1,11 +1,12 @@
 package com.hazarbelge.addresses.repository
 
 import com.hazarbelge.addresses.model.City
+import com.hazarbelge.addresses.model.Country
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CityRepository  : CrudRepository<City, Long> {
     fun findByName(name: String): City
-    fun findAllByOrderByNameAsc(): Iterable<City>
+    fun findAllByCountryOrderByNameAsc(country: Country): Iterable<City>
 }
